c5661047ad1eed7d466ee74894911f4d966a0ba6
Tommi Ronkainen <tommi.ronkainen@gmail.com>
2021-03-29 22:33:11 +0300

    Export also types.

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,4 +1,5 @@
 import { Origin } from './origin';
+export { Origin, OriginType, WebOrigin } from './origin';
 export declare function getOrigin(): Origin;
 declare const _default: {
     getOrigin: typeof getOrigin;
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import { Origin } from './origin';
 
+export { Origin, OriginType, WebOrigin } from './origin';
+
 export function getOrigin(): Origin {
     return { type: 'web', referrer: 'http://localhost' }
 }
