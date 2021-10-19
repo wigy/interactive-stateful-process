1ad52b0c5c6ab724be8a05f4c1d0617bfadceaa6
Tommi Ronkainen <tommi.ronkainen@gmail.com>
2021-10-19 19:49:40 +0300

    Fix migration.

--- migrations/01_init.ts
+++ migrations/01_init.ts
@@ -1,47 +1,45 @@
-import Knex from 'knex'
-
-export async function up (knex) {
+export async function up (knex): Promise<void> {
     await knex.schema.createTable('processes', function (table) {
-        table.increments('id');
-        table.string('name', 32).notNullable();
-        table.boolean('complete').notNullable().default(false);
-        table.boolean('successful').default(null);
-        table.json('origin');
-        table.integer('currentStep').default(null);
+        table.increments('id')
+        table.string('name', 32).notNullable()
+        table.boolean('complete').notNullable().default(false)
+        table.boolean('successful').default(null)
+        table.json('origin')
+        table.integer('currentStep').default(null)
 
-        table.index(['name']);
-    });
+        table.index(['name'])
+    })
 
     await knex.schema.createTable('process_files', function (table) {
-        table.increments('id');
-        table.integer('processId').notNullable();
-        table.foreign('processId').references('processes.id');
+        table.increments('id')
+        table.integer('processId').notNullable()
+        table.foreign('processId').references('processes.id')
         table.text('name')
         table.text('encoding')
         table.text('data')
 
-        table.index(['processId']);
-    });
+        table.index(['processId'])
+    })
 
     await knex.schema.createTable('process_steps', function (table) {
-        table.increments('id');
+        table.increments('id')
 
-        table.integer('processId').notNullable();
-        table.foreign('processId').references('processes.id');
+        table.integer('processId').notNullable()
+        table.foreign('processId').references('processes.id')
 
-        table.integer('number').notNullable();
+        table.integer('number').notNullable()
         table.json('directions')
         table.json('action').default(null)
         table.datetime('started').defaultTo(knex.fn.now())
         table.json('state').notNullable()
         table.datetime('finished').default(null)
 
-        table.index(['processId', 'number']);
-    });
+        table.index(['processId', 'number'])
+    })
 }
 
-export async function down (knex) {
-    await knex.schema.dropTable('process_files');
-    await knex.schema.dropTable('process_steps');
-    await knex.schema.dropTable('processes');
+export async function down (knex): Promise<void> {
+    await knex.schema.dropTable('process_files')
+    await knex.schema.dropTable('process_steps')
+    await knex.schema.dropTable('processes')
 }
