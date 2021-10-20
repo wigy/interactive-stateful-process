e55652f48bdd4d56a88c04b850b60970082d4e8a
Tommi Ronkainen <tommi.ronkainen@gmail.com>
2021-10-20 21:42:06 +0300

    Handle JSON encoding.

--- src/process.ts
+++ src/process.ts
@@ -52,14 +52,17 @@ export class ProcessFile {
    * Save the file to the database.
    */
   async save(db: Database): Promise<ID> {
-    // TODO: Handle JSON encoding, if used.
+    const out = this.toJSON()
+    if (this.encoding === 'json') {
+      out.data = JSON.stringify(out.data)
+    }
     if (this.id) {
-      await db('process_files').update(this.toJSON()).where({ id: this.id })
+      await db('process_files').update(out).where({ id: this.id })
       return this.id
     } else {
-      this.id = (await db('process_files').insert(this.toJSON()).returning('id'))[0]
+      this.id = (await db('process_files').insert(out).returning('id'))[0]
       if (this.id) return this.id
-      throw new DatabaseError(`Saving process ${JSON.stringify(this.toJSON)} failed.`)
+      throw new DatabaseError(`Saving process ${JSON.stringify(out)} failed.`)
     }
   }
 
