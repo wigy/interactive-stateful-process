93658b245332c753d04bf31164c4c63f39b3de0c
Tommi Ronkainen <tommi.ronkainen@gmail.com>
2021-11-18 20:33:03 +0200

    Update.

--- dist/src/index.d.ts
+++ dist/src/index.d.ts
@@ -1,6 +1,5 @@
 export * from './common';
 export * from './error';
-export * from './implementations';
 export * from './process';
 export * from './directions';
 export * from './server';
--- dist/src/index.js
+++ dist/src/index.js
@@ -12,7 +12,6 @@ var __exportStar = (this && this.__exportStar) || function(m, exports) {
 Object.defineProperty(exports, "__esModule", { value: true });
 __exportStar(require("./common"), exports);
 __exportStar(require("./error"), exports);
-__exportStar(require("./implementations"), exports);
 __exportStar(require("./process"), exports);
 __exportStar(require("./directions"), exports);
 __exportStar(require("./server"), exports);
