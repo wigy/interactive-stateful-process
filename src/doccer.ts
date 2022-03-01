b54196f6d76f136e4126a9dd54eaa45f25551db3
Tommi Ronkainen <tommi.ronkainen@gmail.com>
2022-03-01 19:20:23 +0200

    Add note.

--- src/doccer.ts
+++ src/doccer.ts
@@ -9,6 +9,6 @@
  * @module interactive-stateful-process
  */
 
-/** @hidden */
+/** @hidden Dummy definition to force inclusion of this file to documentation */
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const __interactive_stateful_process = null
