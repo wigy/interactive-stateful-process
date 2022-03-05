df034a024f3aed6badc5589ac1e8ab063b656965
Tommi Ronkainen <tommi.ronkainen@gmail.com>
2022-03-05 18:01:09 +0200

    Doc update

--- src/doccer.ts
+++ src/doccer.ts
@@ -6,9 +6,17 @@
  * states, but the transition from one state to another could need user input from
  * the client - e.g. web browser.
  *
+ * ## Sub-modules
+ *
+ * * [common](interactive_stateful_process_src_common.html)
+ * * [error](interactive_stateful_process_src_error.html)
+ * * [import](interactive_stateful_process_src_import.html)
+ * * [process](interactive_stateful_process_src_process.html)
+ * * [server](interactive_stateful_process_src_server.html)
+ *
  * @module interactive-stateful-process
  */
 
 /** @hidden Dummy definition to force inclusion of this file to documentation */
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-const __interactive_stateful_process = null
+const __interactiveStatefulProcess = null
