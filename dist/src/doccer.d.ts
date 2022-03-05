4075c08a29d6d83440931ca477904826da944adf
Tommi Ronkainen <tommi.ronkainen@gmail.com>
2022-03-05 18:06:31 +0200

    Rebuild.

--- dist/src/doccer.d.ts
+++ dist/src/doccer.d.ts
@@ -6,7 +6,15 @@
  * states, but the transition from one state to another could need user input from
  * the client - e.g. web browser.
  *
+ * ## Sub-modules
+ *
+ * * [common](interactive_stateful_process_src_common.html)
+ * * [error](interactive_stateful_process_src_error.html)
+ * * [import](interactive_stateful_process_src_import.html)
+ * * [process](interactive_stateful_process_src_process.html)
+ * * [server](interactive_stateful_process_src_server.html)
+ *
  * @module interactive-stateful-process
  */
 /** @hidden Dummy definition to force inclusion of this file to documentation */
-declare const __interactive_stateful_process: null;
+declare const __interactiveStatefulProcess: null;
